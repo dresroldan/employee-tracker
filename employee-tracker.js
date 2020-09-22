713584ccc2a59c485a75b386dda956c69751b681
Andres Roldan <dresroldan@gmail.com>
2020-09-22 15:58:09 -0500

    added employee prompts

--- employee-tracker.js
+++ employee-tracker.js
@@ -12,10 +12,72 @@ var connection = mysql.createConnection({
 
     // Your password
     password: "moonsafari91!",
-    database: "emplo"
+    database: "employee_trackerDB"
 });
 
 connection.connect(function(err) {
     if (err) throw err;
-    runSearch();
+
+    console.log("Welcome to Employee Tracker!")
+    MainMenu();
 });
+
+
+function MainMenu() {
+    inquirer
+        .prompt({
+
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View all employees",
+                "View all employees by department",
+                "View all employees by Manager",
+                "Add employee",
+                "Remove employee",
+                "Update employee role",
+                "Update employee manager"
+            ]
+        })
+
+    .then((answer) => {
+
+        switch (answer.action) {
+            case "View all employees":
+                viewAllEmp();
+                break;
+
+            case "View all employees by department":
+                viewAllEmpByDepart();
+                break;
+
+            case "View all employees by manager":
+                viewAllEmpByManager();
+                break;
+
+            case "Add employee":
+                addEmployee();
+                break;
+
+            case "Remove employee":
+                removeEmployee();
+                break;
+
+            case "Update employee role":
+                updateEmployRole();
+                break;
+
+            case "Update employee manager":
+                updateEmployMan();
+                break;
+
+        }
+
+
+
+
+
+    });
+
+};
